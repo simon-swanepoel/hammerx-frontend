28310e8daf7ed3b1ee02a0c102a4031b493b29a4
simon-swanepoel <sdfstudydocs@gmail.com>
2026-09-26 16:23:37 +0200

    Fix regex logic for hex color validation

--- SST-comparator.js
+++ SST-comparator.js
@@ -2282,7 +2282,7 @@ document.addEventListener('DOMContentLoaded', () => {
         btnApplyHex.addEventListener('click', () => {
             let val = inputHex.value.trim();
             if (!val.startsWith('#') && val.length === 6) val = '#' + val;
-            if (/^#[0-9A-F]{6}$/i.test(val) \vert{}\vert{} /^#[0-9A-F]{3}$/i.test(val)) {
+            if (/^#[0-9A-F]{6}$/i.test(val) || /^#[0-9A-F]{3}$/i.test(val)) {
                 document.documentElement.style.setProperty(activeColorTargetVar, val);
                 if (nativeColorWell) nativeColorWell.value = val;
                 isSettingsModified = true;
